969d177fb7e37ee806fa66d513efd8eb05373b27
Noman iqbal <noman.bin.iqbal@piecyfer.com>
2020-12-09 19:09:32 +0500

    new game updated.

--- server/config/socket.js
+++ server/config/socket.js
@@ -4,7 +4,7 @@ const WebSocket = require('ws');
 
 module.exports = function (app) {
   const server = require('http').createServer(app);
-  global.wss = new WebSocket.Server({server, clientTracking: true})
+  global.wss = new WebSocket.Server({server})
   global.wss.on('connection', MyWebsocket.connect);
   global.wss.on('error', function (e) {
     console.log(e);
