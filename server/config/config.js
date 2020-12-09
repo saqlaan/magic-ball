2bc7366de35cfcde3384f009d8e577a012e4ecc9
Noman iqbal <noman.bin.iqbal@piecyfer.com>
2020-12-09 19:15:28 +0500

    remove extra param.

--- server/config/config.js
+++ server/config/config.js
@@ -10,8 +10,6 @@ const envVarsSchema = Joi.object({
     .default('development'),
   SERVER_PORT: Joi.number()
     .default(4040),
-  WEBSOCKET_PORT: Joi.number()
-    .default(5050),
   MONGOOSE_DEBUG: Joi.boolean()
     .when('NODE_ENV', {
       is: Joi.string().equal('development'),
@@ -35,7 +33,6 @@ if (error) {
 const config = {
   env: envVars.NODE_ENV,
   port: envVars.SERVER_PORT,
-  websocketPort: envVars.WEBSOCKET_PORT,
   mongooseDebug: envVars.MONGOOSE_DEBUG,
   jwtSecret: envVars.JWT_SECRET,
   frontend: envVars.MEAN_FRONTEND || 'angular',
--- server/index.js
+++ server/index.js
@@ -10,9 +10,6 @@ if (!module.parent) {
   socketApp.listen(config.port, () => {
     console.info(`server started on port ${config.port} (${config.env})`);
   });
-  // socketApp.listen(config.websocketPort, () => {
-  //   console.info(`websocket started on port ${config.websocketPort} (${config.env})`);
-  // });
 }
 
 module.exports = socketApp;
