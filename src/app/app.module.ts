895c7c38a026e2a44564f71c4447b995b106700e
Saqlain Awan <muhammad.saqlain@piecyfer.com>
2021-01-20 21:27:31 +0500

    file changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NgModule} from '@angular/core';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
